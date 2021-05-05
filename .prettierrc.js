5de3a0c5f990c1727751dada368f8dcb41df30f2
1seok2 <misohaha31@naver.com>
2021-05-06 00:09:57 +0900

    create: initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	singleQuote: true,
+	semi: true,
+	useTabs: false,
+	tabWidth: 2,
+	trailingComma: 'all',
+	printWidth: 60,
+	arrowParens: 'always',
+	orderedImports: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: 'avoid',
+};
