c88035ec1f59ab9a305d4bb5df93f1723996a843
1seok2 <misohaha31@naver.com>
2021-05-09 16:41:38 +0900

    feat: set demo page

---
+++ main.da7f140c859ceafd0980.hot-update.js
@@ -0,0 +1,24 @@
+self["webpackHotUpdatefrontend"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/styles/Sidebar.scss":
+/*!**************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/styles/Sidebar.scss ***!
+  \**************************************************************************************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Invalid CSS after \"  }\": expected \"}\", was \".side-bar-content\"\n        on line 40 of src/styles/Sidebar.scss\n>>   }\n\n   ---^\n");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("296d5bb44bf644311a50")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mcm9udGVuZC93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7O1VBQUEsc0QiLCJmaWxlIjoibWFpbi5kYTdmMTQwYzg1OWNlYWZkMDk4MC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiMjk2ZDViYjQ0YmY2NDQzMTFhNTBcIikiXSwic291cmNlUm9vdCI6IiJ9
