aa4718e583b612b801cf1d84d04db8a51af8dc42
1seok2 <misohaha31@naver.com>
2021-05-13 23:24:38 +0900

    feat: add Observe #2

---
+++ main.7709328b63fb7e27dfcc.hot-update.js
@@ -0,0 +1,24 @@
+self["webpackHotUpdatefrontend"]("main",{
+
+/***/ "./src/scroll/ScrollAnimationItem.tsx":
+/*!********************************************!*\
+  !*** ./src/scroll/ScrollAnimationItem.tsx ***!
+  \********************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/choewonseog/Documents/react-scroll-animation/demo/src/scroll/ScrollAnimationItem.tsx: Unexpected token ','. (40:19)\n\n\u001b[0m \u001b[90m 38 |\u001b[39m         elementTopPosition\u001b[0m\n\u001b[0m \u001b[90m 39 |\u001b[39m     ) {\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 40 |\u001b[39m       console\u001b[33m.\u001b[39mlog(\u001b[33m,\u001b[39m path)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m                    \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 41 |\u001b[39m       setShowScrollAnimation(element\u001b[33m,\u001b[39m duration\u001b[33m,\u001b[39m delay\u001b[33m,\u001b[39m path)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 42 |\u001b[39m       endShowAnimation(element\u001b[33m,\u001b[39m duration)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 43 |\u001b[39m       reAnimate \u001b[33m&&\u001b[39m\u001b[0m\n    at Object._raise (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:807:17)\n    at Object.raiseWithData (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:800:17)\n    at Object.raise (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:761:17)\n    at Object.parseExprListItem (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:12051:14)\n    at Object.parseCallExpressionArguments (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:11102:22)\n    at Object.parseCoverCallAndAsyncArrowHead (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:11012:29)\n    at Object.parseSubscript (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:10948:19)\n    at Object.parseSubscript (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:7099:18)\n    at Object.parseSubscripts (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:10921:19)\n    at Object.parseExprSubscripts (/Users/choewonseog/Documents/react-scroll-animation/demo/node_modules/@babel/parser/lib/index.js:10910:17)");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("66d794a5186a898c0322")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mcm9udGVuZC93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7O1VBQUEsc0QiLCJmaWxlIjoibWFpbi43NzA5MzI4YjYzZmI3ZTI3ZGZjYy5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiNjZkNzk0YTUxODZhODk4YzAzMjJcIikiXSwic291cmNlUm9vdCI6IiJ9
